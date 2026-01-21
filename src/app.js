c68c1dbfeed2ba7d3cd67654ef9d83f1458c9520
Ruben Alba Gonzalez <39350323+Ruben-Alba-Gonzalez@users.noreply.github.com>
2026-01-21 19:45:09 +0000

    added domain name generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+function getDomainNames(){
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let domain = ['.com', '.net', '.us', '.io', '.es']
+
+  for(let p of pronoun){
+    for(let a of adj){
+      for(let n of noun){
+        for(let d of domain){
+          console.log(p+a+n+d);
+        }
+      }
+    }
+  }
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  getDomainNames();
 };
